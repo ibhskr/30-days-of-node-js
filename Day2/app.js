a346e0cbca5af42538335859158d26babd2048f4
Bhasakr Roy <i.bhskr@hotmail.com>
2024-02-04 23:24:15 +0530

    Day2

--- Day2/app.js
+++ Day2/app.js
@@ -1,17 +1,16 @@
-import fs from 'fs';
+import fs from "fs";
 
-
-const filePath = "./test-files/output1.txt"
-
-function writeToFile(filePath, content){
-    fs.writeFile(filePath,content,(err)=>{
-        if(err){
-            console.log(err)
-        }else{
-            console.log("file writen sucessfully")
-        }
-    })
+function writeToFile(filePath, content) {
+  fs.writeFile(filePath, content, (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("file writen sucessfully");
+    }
+  });
 }
-writeToFile('test-files/output1.txt', 'Sample content.');
-
-writeToFile('test-files/nonexistent-folder/output.txt', 'Content in a non-existent folder.');
+writeToFile("test-files/output1.txt", "Sample content.");
+writeToFile(
+  "test-files/nonexistent-folder/output.txt",
+  "Content in a non-existent folder."
+);
