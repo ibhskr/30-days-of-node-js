d97dd3f3353d009b5a12ea59ca637689514d949f
Bhasakr Roy <i.bhskr@hotmail.com>
2024-02-04 23:25:02 +0530

    Day4

--- Day4/app.js
+++ Day4/app.js
@@ -1,9 +1,10 @@
 import path from 'path';
 
+
 function resolvePath(relativePath) {
   const absolutePath = path.resolve(relativePath);
   console.log(`Resolved Path: ${absolutePath}`);
-}
+};
 
 // Test cases
 resolvePath('../project/folder/file.txt');
