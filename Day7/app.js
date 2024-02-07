1e01988fd43220b024c1facaf26e5bcd9a84030e
Bhasakr Roy <i.bhskr@hotmail.com>
2024-02-08 00:59:47 +0530

    update

---
+++ Day7/app.js
@@ -0,0 +1,23 @@
+import express from "express";
+const app = express();
+
+function requestLoggerMiddleware(req, res, next) {
+  const timestamp = new Date().toUTCString();
+  const method = req.method;
+  console.log(`${timestamp} - ${method} request received`);
+  next();
+}
+
+
+app.use(requestLoggerMiddleware);
+
+
+app.get("/",(req,res)=>{
+    console.log('req "/" route')
+    res.send('hello')
+})
+// Start the server
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
